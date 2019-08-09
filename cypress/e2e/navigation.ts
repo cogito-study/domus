7e0dd689f4341e72a22cb961fbb9f68264b9b209
Mate Papp <matepapp@icloud.com>
2019-08-09 17:41:27 +0200

    Add initial project from LekoArts portfolio Jodie

---
+++ cypress/e2e/navigation.ts
@@ -0,0 +1,47 @@
+/// <reference types="../support/index" />
+/// <reference types="cypress" />
+/// <reference types="@testing-library/cypress/typings" />
+
+describe('navigation', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('link to projects works', () => {
+    cy.get('nav').within(() => {
+      cy.getByText(/projects/i)
+        .click({ force: true })
+        .waitForRouteChange()
+    })
+    cy.getByLabelText(/view project "Color"/i).assertRoute('/projects')
+  })
+  it('link to instagram works', () => {
+    cy.get('nav').within(() => {
+      cy.getByText(/instagram/i)
+        .click({ force: true })
+        .waitForRouteChange()
+    })
+    cy.assertRoute('/instagram')
+  })
+  it('link to about works', () => {
+    cy.get('nav').within(() => {
+      cy.getByText(/about/i)
+        .click({ force: true })
+        .waitForRouteChange()
+    })
+    cy.getByText(/Hi. I'm LekoArts!/i).assertRoute('/about')
+  })
+  it('link to home works', () => {
+    cy.get('nav').within(() => {
+      cy.getByText(/about/i)
+        .click({ force: true })
+        .waitForRouteChange()
+    })
+    cy.getByText(/Hi. I'm LekoArts!/i)
+      .assertRoute('/about')
+      .getByLabelText(/LekoArts, Back to home/i)
+      .click({ force: true })
+      .waitForRouteChange()
+      .assertRoute('/')
+  })
+})
---
+++ cypress/e2e/project.ts
@@ -0,0 +1,27 @@
+/// <reference types="../support/index" />
+/// <reference types="cypress" />
+/// <reference types="@testing-library/cypress/typings" />
+
+describe('project', () => {
+  beforeEach(() => {
+    cy.visit('/')
+      .getByLabelText(/view project "breakfast"/i)
+      .click({ force: true })
+      .waitForRouteChange()
+  })
+
+  it('should be linked from the index page', () => {
+    cy.assertRoute('/breakfast')
+  })
+  it('should have a category, title, description', () => {
+    cy.getByText(/photography/i)
+      .getByText(/Breakfast - The most important time of the day/i)
+      .getByText(/The first meal of the day./i)
+  })
+  it('should have images', () => {
+    cy.getByAltText(/projects-breakfast-003/i)
+  })
+  it('should have a contact button', () => {
+    cy.getByText(/contact us/i)
+  })
+})

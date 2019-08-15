8ee8b7b586251f1fafac68612c54d30787ae1a46
kiralybalint <kiraly.balint@simonyi.bme.hu>
2019-08-15 18:03:48 +0200

    Fix spacing

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react';
 
 export const Container: FunctionComponent = ({ children }) => {
   return (
-    <Box p={["0 5px", "0 20px", "0"]} m={["0 20px","0 auto"]} maxWidth={["100%", "640px", "640px", "1024px"]}>
+    <Box px={[1, 4, 0]} mx="auto" maxWidth={["100%", "640px", "640px", "1024px"]}>
       {children}
     </Box>
   );

c0f8a1fc4deecf5cbf3e6bc02384d8c516b688e0
Lobli <lobli@icloud.com>
2019-08-17 12:39:08 +0200

    Updated layout

--- src/components/sections/try-out.section.tsx
+++ src/components/sections/try-out.section.tsx
@@ -2,6 +2,7 @@ import { Flex, H2, Paragraph } from '@cogito-study/alea';
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { TryOutQuery } from '../../generated/graphql-types';
+import { Container } from '../container';
 
 export const TryOutSection = () => {
   const data = useStaticQuery<TryOutQuery>(graphql`
@@ -39,20 +40,22 @@ export const TryOutSection = () => {
           const { image, image_description, subtitle, title } = primary;
 
           return (
-            <>
-              <Flex flexDirection="column">
+            <Container>
+              <Flex flexDirection="row" my={6} width="900px" justifyContent="space-between" alignItems="center">
+              <Flex flexDirection="column" maxWidth="300px">
                 {image && image.url && <img src={image.url} alt={image.alt || ''} />}
-                {image_description && <Paragraph color="grey.light.4">{image_description.text}</Paragraph>}
+                {image_description && <Paragraph my={1} color="grey.light.4" textAlign="center" mr={7}>{image_description.text}</Paragraph>}
               </Flex>
-              <Flex flexDirection="column">
-                {title && <H2 color="grey.light.4">{title.text}</H2>}
+              <Flex flexDirection="column" maxWidth="420px" mx={3}>
+                {title && <H2 my={2} color="grey.light.4">{title.text}</H2>}
                 {subtitle && (
-                  <Paragraph paragraphSize="large" color="grey.light.4">
+                  <Paragraph paragraphSize="large" my={2} color="grey.light.4">
                     {subtitle.text}
                   </Paragraph>
                 )}
               </Flex>
-            </>
+              </Flex>
+            </Container>
           );
         }
       })}

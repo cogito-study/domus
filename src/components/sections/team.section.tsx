7c00e16dd771e0b2d33486def3f6a1cdf62cf558
kiralybalint <kiraly.balint@simonyi.bme.hu>
2019-08-17 21:32:49 +0200

    Init about us page

---
+++ src/components/sections/team.section.tsx
@@ -0,0 +1,61 @@
+import React, { FunctionComponent } from 'react';
+import { Box, H1, H2, H3, Grid, Paragraph } from '@cogito-study/alea';
+import { graphql, useStaticQuery } from 'gatsby';
+import { TeamQuery } from '../../generated/graphql-types';
+import { Container } from '../container';
+
+interface TeamSectionProps {
+  title: string;
+  subtitle: string;
+}
+
+export const TeamSection: FunctionComponent<TeamSectionProps> = ({ title, subtitle }) => {
+  const data = useStaticQuery<TeamQuery>(graphql`
+    query Team {
+      allPrismicAboutBodyProfile {
+        edges {
+          node {
+            primary {
+              image {
+                url
+              }
+              name {
+                text
+              }
+              position {
+                text
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (!data.allPrismicAboutBodyProfile) return null;
+
+  return (
+    <Container>
+      <H1 color="primary.dark">{title}</H1>
+      <H2 color="grey.dark.3">{subtitle}</H2>
+      <Grid
+        gridGap={5}
+        gridTemplateColumns={['1fr 1fr', '1fr 1fr 1fr 1fr', '1fr 1fr 1fr 1fr', '1fr 1fr 1fr']}
+      >
+        {data.allPrismicAboutBodyProfile.edges.map(({ node: { primary } }) => (
+          <Box>
+            <Box height={["140px", "140px", "140px", "300px"]}> 
+              <img src={primary.image.url} width="100%" height="100%" style={{ objectFit: 'cover' }} />
+            </Box>
+            <H3 mb={1} mt={2} color="primary.dark">
+              {primary.name.text}
+            </H3>
+            <Paragraph mb={6} mt={0} color="grey.normal">
+              {primary.position.text}
+            </Paragraph>
+          </Box>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,52 @@
 import React from 'react'
 import { Layout } from '../components/layout'
 import SEO from '../components/SEO'
+import { graphql } from 'gatsby';
+import {
+  TeamSection,
+  AchievementSection,
+  CustomerSection,
+  PartnerSection,
+  TryOutSection,
+} from '../components/sections';
 
-const About = () => {
+const About: FunctionComponent<AboutProps> = ({ data }) => {
+
+  const { node } = data.allPrismicAbout.edges[0];
+  const { title, subtitle, achievement_header } = node.data;
+
   return (
     <Layout>
-      <SEO
-        title="About | Jodie"
-        desc="Hi. I'm LekoArts! You can visit my website or my other Gatsby projects."
-      />
+      <SEO/>
+      <TeamSection title={title.text} subtitle={subtitle.text}/>
+      <AchievementSection title={achievement_header.text} />
+      <CustomerSection title="" />
+      <PartnerSection />
+      <TryOutSection />
     </Layout>
   )
 }
 
+export const query = graphql`
+  query About {
+    allPrismicAbout {
+      edges {
+        node {
+          lang
+          data {
+            title {
+              text
+            }
+            subtitle {
+              text
+            }
+            achievement_header {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 export default About

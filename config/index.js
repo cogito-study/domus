125e9811db183fcb187e1172e6fc51dc193a58ab
Lobli <lobli@icloud.com>
2019-08-18 18:11:37 +0200

    Updated data

--- config/index.js
+++ config/index.js
@@ -1,20 +1,20 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Cogito', // Navigation and Site Title
+  siteTitleAlt: 'Cogito - Collaborative e-learning platform', // Alternative Site title for SEO
+  siteTitleShort: 'Cogito', // short_name for manifest
+  siteHeadline: 'Make education a community, not just an institute!', // Headline for schema.org JSONLD
+  siteUrl: 'https://cogito.study', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Collaborative course management for reliable online materials and inspiration.',
+  author: 'Cogito', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
-  ogLanguage: 'en_US', // og:language
+  userTwitter: '@cogitostudy', // Twitter Username
+  ogSiteName: 'cogito', // Facebook Site Name
+  ogLanguage: 'en_UK', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 
   // Manifest and Progress color

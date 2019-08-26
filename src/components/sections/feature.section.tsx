97511ce63a8a5edac98f3b6b00138c8ed6805b99
Lobli <lobli@icloud.com>
2019-08-26 18:16:49 +0200

    Reversed second feature item

--- src/components/sections/feature.section.tsx
+++ src/components/sections/feature.section.tsx
@@ -30,26 +30,27 @@ export const FeatureSection = () => {
   if (!data.allPrismicHomeBodyFeature) return null;
 
   return (
-    <Flex flexDirection="column" alignItems={['center', 'center', 'space-between']}           mx={[4, 4, 0, 7]}
-    >
+    <Flex flexDirection="column" alignItems={['center', 'center', 'space-between']} mx={[4, 4, 0, 7]}>
       {data.allPrismicHomeBodyFeature.edges.map(({ node: { primary } }, index) => (
         <Flex
           key={index}
-          flexDirection={['column', 'column', 'row', 'row']}
+          flexDirection={
+            index % 2 == 1 ? ['column', 'column', 'row-reverse', 'row-reverse'] : ['column', 'column', 'row', 'row']
+          }
           alignItems={['center', 'center', 'space-between']}
           justifyContent={['center', 'center', 'space-between']}
           my={[4, 8, 7]}
           width={['100%', '70%', '100%', '100%']}
         >
-          {/* Width should be 300px on smaller screens and 400px on desktop */}
-          <Box  width={['300px', '300px', '300px', '400px']}>
-          {primary && primary.image && <img alt={primary.image.alt} src={primary.image.url} width="100%" />}
+          {/* Width should be 300px on smaller screens and 400px on desktop on the image */}
+          <Box width={['300px', '300px', '300px', '400px']}>
+            {primary && primary.image && <img alt={primary.image.alt} src={primary.image.url} width="100%" />}
           </Box>
           <Flex
             flexDirection="column"
             justifyContent="center"
             maxWidth={['100%', '100%', '300px', '470px']}
-            ml={[0, 0, 0, 9]}
+            ml={index % 2 == 1 ?[0]: [0, 0, 0, 9]}
             mt={[4, 6, 7, 0]}
           >
             {primary && primary.title && (

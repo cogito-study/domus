fd5121a3e5489afd59d714d6b7f25848257c5e4e
Lobli <lobli@icloud.com>
2019-08-29 09:13:00 +0000

    Larger horizontal padding on mobile

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react';
 
 export const Container: FunctionComponent = ({ children }) => {
   return (
-    <Box px={[1, 4, 0]} mx="auto" maxWidth={["100%", "640px", "640px", "1024px"]}>
+    <Box px={[4, 4, 0]} mx="auto" maxWidth={['100%', '640px', '640px', '1024px']}>
       {children}
     </Box>
   );

bc755556dfe690d9e841ffb330059923afa25e05
Ádám Belákovics <beladi96@gmail.com>
2019-09-02 19:32:04 +0200

    Add menu control

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,5 +1,5 @@
 import { Anchor, Box, Button, Flex, theme } from '@cogito-study/alea';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 import { StyledGatsbyLink } from './styled/styled-gatsby-link';
@@ -13,6 +13,9 @@ const pages: Record<string, string> = {
 };
 
 const NavbarContainerLarge = styled(Flex)`
+  position: fixed;
+  top: 0;
+  width: 98%;
   @media (max-width: 1023px) {
     display: none;
   }
@@ -23,12 +26,27 @@ const NavbarContainerLarge = styled(Flex)`
 `;
 
 const NavbarContainerSmall = styled(Flex)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+
   @media (min-width: 1024px) {
     display: none;
   }
 `;
 
+const OverlayMenu = styled(Flex)`
+  position: fixed;
+  top: 62px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  width: 100vw;
+  height: 100vh;
+`;
+
 export const NavBar: FunctionComponent = () => {
+  const [overlayMenuActive, setOverlayMenuActive] = useState(false);
   return (
     <>
       <NavbarContainerLarge
@@ -38,7 +56,6 @@ export const NavBar: FunctionComponent = () => {
         borderColor="primary.light"
         mt={3}
         backgroundColor="white"
-        style={{ position: 'fixed', top: 0, width: '98%' }}
       >
         <Flex alignItems="center">
           <Link to="/">
@@ -74,16 +91,46 @@ export const NavBar: FunctionComponent = () => {
         borderBottom="1px solid"
         borderColor="primary.light"
         backgroundColor="white"
-        style={{ position: 'fixed', top: 0, width: '100%' }}
       >
         <Link to="/">
           <Flex mx={8} my={3}>
             <img src="/logos/logo.svg"></img>
           </Flex>
         </Link>
-        <Box mr={4}>
-          <img src="/logos/menu.svg"></img>
+        <Box mr={4} onClick={() => setOverlayMenuActive(!overlayMenuActive)}>
+          {!overlayMenuActive && <img src="/logos/menu.svg"></img>}
+          {overlayMenuActive && <img src="/logos/close.svg"></img>}
         </Box>
+        {overlayMenuActive && (
+          <OverlayMenu flexDirection="column" alignItems="center" alignContent="space-between" backgroundColor="white">
+            <Flex flexDirection="column" flexGrow={2} justifyContent="center">
+              {Object.keys(pages).map((pageName, index) => (
+                <Box key={index} height="22px" width="98px" textAlign="center" mr={4}>
+                  <StyledGatsbyLink
+                    to={pages[pageName]}
+                    activeClassName="active"
+                    color={'grey.dark.1'}
+                    hoverColor="grey.dark.4"
+                    fontWeight="bold"
+                  >
+                    {pageName}
+                  </StyledGatsbyLink>
+                </Box>
+              ))}
+            </Flex>
+            <Flex flexDirection="column" style={{ position: 'fixed', bottom: 0 }}>
+              <Button variant="secondary" mb={3}>
+                log in
+              </Button>
+              <Button mb={3}>try out!</Button>
+              <Flex mb={2}>
+                <Anchor mr={2}>English</Anchor>
+                <Anchor mr={2}>Hungarian</Anchor>
+                <Anchor mr={2}>Romanian</Anchor>
+              </Flex>
+            </Flex>
+          </OverlayMenu>
+        )}
       </NavbarContainerSmall>
     </>
   );

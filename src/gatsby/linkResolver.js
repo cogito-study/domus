fd4a3884ad7159d49153fc13bf867d8cf965a0c9
Mate Papp <matepapp@icloud.com>
2019-09-03 13:11:49 +0200

    Remove unused files and configs

--- src/gatsby/linkResolver.js
+++
@@ -1,9 +0,0 @@
-const i18n = require('../../config/i18n')
-
-const linkResolver = doc => {
-  const prefix = i18n[doc.lang].default ? `/` : `/${i18n[doc.lang].path}/`
-
-  return `${prefix}${doc.uid}`
-}
-
-module.exports = linkResolver

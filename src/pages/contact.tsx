714f3c5dbf76f024ac0cc2560a8f409854b1e972
Lobli <lobli@icloud.com>
2019-09-03 20:29:32 +0200

    Finalize contact section

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { Layout } from '../components/layout';
 import SEO from '../components/SEO';
-import { CustomerSection, PartnerSection, TryOutSection, ContactSection } from '../components/sections';
+import { PartnerSection, TryOutSection, ContactSection } from '../components/sections';
 
 const Contact: FunctionComponent = () => {
   return (
     <Layout>
       <SEO />
       <ContactSection />
-      <CustomerSection title="" />
       <PartnerSection />
       <TryOutSection />
     </Layout>

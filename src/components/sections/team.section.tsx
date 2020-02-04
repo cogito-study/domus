68f1dbe0dc0ebfa89cbd30b96c2d260b6d2c1c71
szucsdani <szucsdaniel97@gmail.com>
2020-02-04 17:16:25 +0100

    Implement team.section-to-chakra

--- src/components/sections/team.section.tsx
+++ src/components/sections/team.section.tsx
@@ -1,8 +1,8 @@
-import { Grid, H1, H2, Flex } from '@cogito-study/alea';
+import { Flex, Grid, Heading } from '@chakra-ui/core';
 import { graphql, useStaticQuery } from 'gatsby';
-import { TeamMember } from '../team-member';
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
+import { TeamMember } from '../team-member';
 
 const StyledGrid = styled(Grid)`
   justify-items: start;
@@ -56,18 +56,21 @@ export const TeamSection: FunctionComponent<TeamSectionProps> = ({ title, subtit
 
   return (
     <Flex
-      flexDirection="column"
-      justifyContent="start"
+      direction="column"
+      justify="start"
       px={[0, 4, 4, 4, 0]}
-      maxWidth={['100%', '640px', '640px', '1024px']}
+      maxW={['100%', '640px', '640px', '1024px']}
     >
-      <H1 color="primary.8" mb={0}>
+      <Heading as="h1" color="blue.800" mb={0} fontSize={['xl', 'xl', '2xl']} lineHeight="normal">
         {title}
-      </H1>
-      <H2 color="neutral.8" mt={4} mb={7}>
+      </Heading>
+      <Heading color="grey.800" mt={4} mb={12} fontSize={['lg', 'lg', 'xl']}>
         {subtitle}
-      </H2>
-      <StyledGrid gridGap={[3, 3, 4, 5, 6]} gridTemplateColumns={['1fr 1fr', '1fr 1fr', '1fr 1fr 1fr']}>
+      </Heading>
+      <StyledGrid
+        gridGap={[3, 3, 4, 5, 6]}
+        gridTemplateColumns={['1fr 1fr', '1fr 1fr', '1fr 1fr 1fr']}
+      >
         {data.allPrismicAboutBodyProfile.edges.map((profile: any, index: number) => {
           const { original_image, hovered_image1, name, position } = profile.node.primary;
 

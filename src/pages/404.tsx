98b3abbd766fb17858bdca36836cf4de650504b3
kristofkovacs <kovacsegrikristof@gmail.com>
2020-02-11 12:27:18 +0100

    PR suggests implemented

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,7 +12,7 @@ const Notfound: FunctionComponent = () => {
         align="center"
         justify="center"
         direction={['column-reverse', 'column-reverse', 'column-reverse', 'row']}
-        mt={10}
+        mt={16}
       >
         <Flex w={['250px', '250px', '350px', '550px']} h={['250px', '250px', '350px', '550px']}>
           <Image src={travolta} w="100%" h="100%" />
@@ -42,25 +42,18 @@ const Notfound: FunctionComponent = () => {
           </Flex>
           <Flex>
             <Link to="/">
-              <Button
-                borderRadius={0}
-                variantColor="teal"
-                bg="teal.500"
-                color="blue.800"
-                mr={4}
-                fontSize="sm"
-              >
+              <Button borderRadius={0} variantColor="teal" color="blue.800" mr={4}>
                 back to home
               </Button>
             </Link>
             <Link to="/contact">
               <Button
                 borderRadius={0}
+                variant="outline"
+                variantColor="teal"
                 borderColor="teal.500"
                 borderWidth={2}
                 color="blue.800"
-                bg="fff"
-                fontSize="sm"
               >
                 contact us
               </Button>

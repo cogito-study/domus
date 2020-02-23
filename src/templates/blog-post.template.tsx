df4192b830c95e51f84f5fa71a19ef1bc7559ed3
kristofkovacs <kovacsegrikristof@gmail.com>
2020-02-23 10:43:35 +0200

    SEO Facebook og image tryout

--- src/templates/blog-post.template.tsx
+++ src/templates/blog-post.template.tsx
@@ -1,16 +1,37 @@
 import { Box, Flex, Heading } from '@chakra-ui/core';
 import { graphql } from 'gatsby';
 import React, { FunctionComponent } from 'react';
-import SEO from '../components/SEO';
 import { RelatedBlogPostSlices } from '../components/slices/related-blog-post.slices';
 import { StyledContent } from '../components/styled/styled-content';
+import { GatsbySeo } from 'gatsby-plugin-next-seo';
 
 const BlogPostTemplate: FunctionComponent<{ data: any }> = ({ data }) => {
   const { title, hero_image, content, body } = data.prismicBlogPost.data;
 
   return (
     <>
-      <SEO individual title={`${title.text.toLowerCase()} | blog`} banner={hero_image.url} />
+      <GatsbySeo
+        title={title}
+        description={`${content.substring(0, 100)}...`}
+        canonical="https://cogito.study/blog"
+        openGraph={{
+          url: 'https://cogito.study/blog',
+          title: title,
+          description: `${content.substring(0, 100)}...`,
+          images: [
+            {
+              url: hero_image,
+              alt: 'Og Image Alt',
+            },
+          ],
+          site_name: `${title} | blog | cogito`,
+        }}
+        twitter={{
+          handle: '@cogitostudy',
+          site: '@cogitostudy',
+          cardType: 'summary_large_image',
+        }}
+      />
       <Box position="fixed" h="100vh" w="100vw" bg="#fff" zIndex={-1} opacity={0.35} />
       <Flex justify="center" direction="column" align="center" mb={6}>
         <Box mt={[24, 32]} width="100%" maxW="580px" bg="transparent">

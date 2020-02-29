9f9394912cde65ff5d6d958e917f9787fa2d54f0
kristofkovacs <kovacsegrikristof@gmail.com>
2020-02-29 19:15:01 +0200

    Fix base path problem

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 const config = require('./config');
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+const pathPrefix = config.pathPrefix === '/en' ? '' : config.pathPrefix;
 
 module.exports = {
   pathPrefix: config.pathPrefix,
